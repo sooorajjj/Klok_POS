baa3c7dd8f7b611d27263c5a2529ca7b2658c085
sooorajjj <sooorajjj@gmail.com>
2016-07-15 17:15:29 +0530

    1st try compiling machine code     *will not build     *needs to correct cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
-int main(int argc, char const *argv[])
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SQLiteCpp/SQLiteCpp.h> 
+
+int main(int argc, const char* argv[])
 {
-	return 0;
+   try
+{
+    // Open a database file
+    SQLite::Database    db("PayCollect.db");
+
+    // Compile a SQL query, containing one parameter (index 1)
+    SQLite::Statement   query(db, "SELECT * FROM pay_coll_user ");
+
+    // Bind the integer value 6 to the first parameter of the SQL query
+    // Loop to execute the query step by step, to get rows of result
+    while (query.executeStep())
+    {
+        // Demonstrate how to get some typed column value
+        int         id      = query.getColumn(0);
+        const char* value   = query.getColumn(1);
+
+        std::cout << "row: " << id << ", " << value << ", "<< std::endl;
+    }
 }
+catch (std::exception& e)
+{
+    std::cout << "exception: " << e.what() << std::endl;
+}
+}
+
 
