65b780063fc1fd72e6a1155940ca0fa19781f404
sooorajjj <sooorajjj@gmail.com>
2016-07-15 17:56:56 +0530

    Add: Visiontek libraries and headers    * Build fails ATM - cmake needs fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,16 @@
 #include <stdlib.h>
 #include <SQLiteCpp/SQLiteCpp.h> 
 
+extern "C"{
+	#include<X6x8.h>
+	#include<0202lcd.h>
+}
+
 int main(int argc, const char* argv[])
 {
-   try
-{
+	lk_open();
+	try
+	{
     // Open a database file
     SQLite::Database    db("PayCollect.db");
 
@@ -16,19 +22,19 @@ int main(int argc, const char* argv[])
 
     // Bind the integer value 6 to the first parameter of the SQL query
     // Loop to execute the query step by step, to get rows of result
-    while (query.executeStep())
-    {
-        // Demonstrate how to get some typed column value
-        int         id      = query.getColumn(0);
-        const char* value   = query.getColumn(1);
+	    while (query.executeStep())
+	    {
+	        // Demonstrate how to get some typed column value
+	        int         id      = query.getColumn(0);
+	        const char* value   = query.getColumn(1);
 
-        std::cout << "row: " << id << ", " << value << ", "<< std::endl;
-    }
-}
-catch (std::exception& e)
-{
+	        std::cout << "row: " << id << ", " << value << ", "<< std::endl;
+	    }
+	}
+	catch (std::exception& e)
+	{
     std::cout << "exception: " << e.what() << std::endl;
-}
+	}
 }
 
 
